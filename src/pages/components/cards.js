950ac0ef78d8957413579a1be11a9cbef8bdd850
10o4d <149402618+10o4d@users.noreply.github.com>
2025-07-03 13:42:38 +0000

    Add atualizações

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -4,12 +4,14 @@ export default function Cards(noticia) {
     return <>
         <Col key={noticia.idnoticia}>
             <Card>
-                <Card.Header className="text-center fw-bold bg-warning-subtle">{noticia.titulonoticia}</Card.Header>
+                <Card.Header className="text-center fw-bold bg-warning-subtle">
+                    <a href={`/noticias/${noticia.idnoticia}`}>{noticia.titulonoticia}</a>
+                    </Card.Header>
                 <Card.Body>
                     <Card.Title className="text-capitalize">{noticia.tiponoticia}</Card.Title>
                     <Card.Text> {noticia.conteudonoticia}</Card.Text>
                 </Card.Body>
-                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString ("pt-br") }</Card.Footer>
+                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString("pt-br")}</Card.Footer>
             </Card>
         </Col>
     </>
